50ea0f55250375e923c61508cc67ecc0ae3259c8
Mandira Basnet <mandeerab20@gmail.com>
2024-02-26 23:03:56 +0545

    feat: :lipstick: global prisma instance and issue post api

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,7 +1,8 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
+import prisma from "@/prisma/client";
 
- const createIssueSchema = z.object({
+const createIssueSchema = z.object({
   title: z.string().min(1).max(255),
   description: z.string().min(1),
 });
@@ -9,4 +10,22 @@ import { z } from "zod";
 export async function POST(request: NextRequest) {
   const body = await request.json();
   //this method parses the JSON-formatted body into a JavaScript object
+  const validation = createIssueSchema.safeParse(body);
+
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, {
+      status: 400,
+      statusText: "Bad request",
+    });
+
+ 
+
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(newIssue, { status: 201 });
 }
